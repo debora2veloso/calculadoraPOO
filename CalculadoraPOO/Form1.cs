d73854744bfb3ca32c4e2fbf3ed75a4d8f1a147f
debora2veloso <deborafilipa321@gmail.com>
2019-11-12 20:42:45 +0000

    Add project files.

---
+++ CalculadoraPOO/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculadoraPOO
+{
+    public partial class Form1 : Form
+    {
+        private Calculadora casio = new Calculadora();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clicar(object sender, EventArgs e)
+        {
+            casio.input = ((Button)sender).Tag.ToString();
+            label1.Text = casio.output;
+            memory.Visible = casio.memory;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = casio.output;
+            memory.Visible = false;
+        }
+    }
+}
